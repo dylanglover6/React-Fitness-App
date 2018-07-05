e449e63525995a5dc39dae8389081a44c927cfe6
Daniel Hayes <danielhayespco@gmail.com>
2018-07-05 01:31:18 -0500

    first commit been working on api

--- client/src/components/WorkoutCards/WorkoutCards.js
+++ client/src/components/WorkoutCards/WorkoutCards.js
@@ -1,1 +1,21 @@
 import "./WorkoutCards.css";
+
+<Card>
+  <CardHeader>
+    <CardTitle>Title goes here</CardTitle>
+  </CardHeader>
+  <CardMedia 
+    style={{
+      backgroundImage: 'url("/card_bg.jpg")',
+      height: '300px',
+      backgroundSize: 'cover',
+    }
+  />
+  <CardText>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+  </CardText>
+  <CardActions>
+    <Button compact >action 1</Button>
+    <Button compact >action 2</Button>
+  </CardActions>
+</Card>
--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const workoutsController = require("../../controllers/workoutsController");
 
-
+router.route("/")
+  .get(workoutsController.findAll)
+  .post(workoutsController.create);
 
 module.exports = router;
