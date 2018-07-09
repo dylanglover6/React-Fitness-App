6ff37877a72b33db07c9cac903efdb594358bde4
MK3880 <34534769+MK3880@users.noreply.github.com>
2018-07-09 18:23:49 -0500

    added navpills

---
+++ client/src/components/Navpills/Navpills.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navpills.css";
+
+const Navpills = props => (
+    <div>
+        <ul className="nav nav-pills nav-justified">
+            <p>Workouts</p>
+            <li
+                className={props.message.indexOf('incorrectly') !== -1 ? 
+                    "desc-incorrect" : 
+                    props.message.indexOf('correctly') !== -1 ?
+                        "desc-correct" :
+                        "desc-normal"}
+            >
+                {props.message}
+            </li>
+            <li>Workouts Added: <span style={{color: "yellow"}}>{props.curScore}</span> | Total Workouts: {props.topScore}</li>
+        </ul>
+    </div>
+);
+
+export default Navpills;
