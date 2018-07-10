98e9bf0f9632e431b2a6791618e5a5c8c8cf292a
mhector <maelihector@gmail.com>
2018-07-10 15:39:36 -0500

    Minor formatting/CSS edits.

--- client/src/pages/UserHistory/UserHistory.js
+++ client/src/pages/UserHistory/UserHistory.js
@@ -1,27 +1,31 @@
 import React from "react";
 import { Col, Row, Container } from "../../components/Grid";
 import Jumbotron from "../../components/Jumbotron";
+import Navbar from '../../components/Navbar/Navbar';
 
 const UserHistory = () => (
-  <Container fluid>
-    <Row>
-      <Col size="md-12">
-        <Jumbotron>
-          <h1>User History</h1>
-          <h1>
-            <span>
-            WeekHistory Component Here
-            </span>
-          </h1>
-          <h1>
-            <span>
-            FullWorkoutHistory Component Here
-            </span>
-          </h1>
-        </Jumbotron>
-      </Col>
-    </Row>
-  </Container>
+  <div>
+    <Navbar />
+    <Container fluid>
+      <Row>
+        <Col size="md-12">
+          <Jumbotron>
+            <h1>User History</h1>
+            <h1>
+              <span>
+              WeekHistory Component Here
+              </span>
+            </h1>
+            <h1>
+              <span>
+              FullWorkoutHistory Component Here
+              </span>
+            </h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  </div>
 );
 
 export default UserHistory; 
--- client/src/pages/ViewWorkouts/ViewWorkouts.js
+++ client/src/pages/ViewWorkouts/ViewWorkouts.js
@@ -1,24 +1,29 @@
 import React, { Component } from "react";
 import Jumbotron from "../../components/Jumbotron";
 import { Col, Row, Container } from "../../components/Grid";
+import Navbar from '../../components/Navbar';
+
 class ViewWorkouts extends Component {
   
   render() {
     return (
-      <Container fluid>
-              <Row>
-          <Col size="md-12">
-            <Jumbotron>
-              <h1>All Available Workouts</h1>
-              <h1>
-                <span>
-                ViewWorkouts Component Here
-                </span>
-              </h1>
-            </Jumbotron>
-          </Col>
-        </Row>
-      </Container>
+      <div>
+        <Navbar />
+        <Container fluid>
+                <Row>
+            <Col size="md-12">
+              <Jumbotron>
+                <h1>All Available Workouts</h1>
+                <h1>
+                  <span>
+                  ViewWorkouts Component Here
+                  </span>
+                </h1>
+              </Jumbotron>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
