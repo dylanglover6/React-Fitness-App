b3c015e46c77fbd4745c249c4863aec5e129e7fa
mhector <maelihector@gmail.com>
2018-07-13 11:08:47 -0500

    Edit formatting.

--- client/src/components/User/Account.js
+++ client/src/components/User/Account.js
@@ -6,6 +6,7 @@ import withAuthorization from './withAuthorization';
 import { Link } from 'react-router-dom';
 import * as routes from '../../constants/routes';
 import SignOutButton from '../User/SignOut';
+import { Col, Row, Container } from '../Grid';
 
 const AccountPage = () =>
 <div>
@@ -13,14 +14,24 @@ const AccountPage = () =>
     {authUser =>
       <div>
         <h3>Email: {authUser.email}</h3>
-        <PasswordForgetForm />
-        <PasswordChangeForm />
+        <Col size="sm-12">
+          <Row>
+            <Col size="sm-6">
+              <PasswordForgetForm />
+            </Col>
+            <Col size="sm-6">
+              <PasswordChangeForm />
+            </Col>
+        </Row>
+        </Col>
       </div>
     }
   </AuthUserContext.Consumer>
   <Link to={routes.HOME}>Home</Link>
   <SignOutButton />
 </div>
+
 const authCondition = (authUser) => !!authUser;
 
 export default withAuthorization(authCondition)(AccountPage);
+
